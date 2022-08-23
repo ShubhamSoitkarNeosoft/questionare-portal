d086df48a1b7d2da484b04972460000c2c58b5a7
ShubhamSoitkarNeosoft <shubham.soitkar@neosoftmail.com>
2022-08-23 20:57:14 +0530

    Customized user model and created Client model

---
+++ base/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=255)
+    assigned_date = models.DateTimeField()
+    status = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    interview_date = models.CharField(max_length=255)
+    project_type = models.CharField(max_length=255)
+    status = models.BooleanField()
+    mentor = models.ForeignKey(Mentor, on_delete= models.PROTECT)
+
+    def __str__(self):
+        return f'{self.name}'
